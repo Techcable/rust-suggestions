c0389969d56777073da37f875772723c961ae006
Techcable <Techcable@techcable.net>
2022-01-16 00:41:45 -0700

    Provide some examples of library usage

--- README.md
+++ README.md
@@ -6,6 +6,35 @@ The only dependency is [strsim](https://lib.rs/crates/strsim).
 
 The implementation is copied directly from clap ([see here](https://github.com/clap-rs/clap/blob/7b7c76e3d0279b474c774ea738aecb1d77251df8/src/parse/features/suggestions.rs#L12-L24)). It has just been extracted into a library.
 
+## Examples
+```
+let possible_vals = vec!["test", "possible", "values"];
+let input = "tst";
+let suggestions = suggestions::provide_suggestions(input, &possible_vals);
+assert_eq!(suggestions, vec!["test"]);
+// We have a convenience function to only pick only a single suggestion, giving `Some` or `None`
+let single_suggestion = suggestions::provide_a_suggestion(input, &possible_vals);
+assert_eq!(single_suggestion.unwrap(), "test");
+```
+
+```
+let possible_vals = vec!["testing", "tempo"];
+let input = "teso"; // Sems ambiguous. Maybe multiple suggestions?
+let suggestions = suggestions::provide_suggestions(input, &possible_vals);
+// The implementation trys to order matches from "best" to "wort"
+assert_eq!(suggestions, vec!["testing", "tempo"]);
+// Asking for a single suggestion here would attempt to return the "best" one
+```
+
+If nothing is reasonably similar, asking for suggestions
+will return `vec![]` or `None`.
+
+```
+let possible_vals = vec!["testing", "things", "here"];
+let input = "--something-completely_different";
+assert_eq!(suggestions::provide_a_suggestion(&input, &possible_vals), None)
+```
+
 ## Binary
 A binary is available as an example of how to use the library.
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,51 @@
 //! Minimal library to provide clap-style "Did you mean?" suggestions.
 //!
 //! The implementation is copied directly from clap.
+//!
+//! ## Examples
+//! ```
+//! let possible_vals = vec!["test", "possible", "values"];
+//! let input = "tst";
+//! let suggestions = suggestions::provide_suggestions(input, &possible_vals);
+//! assert_eq!(suggestions, vec!["test"]);
+//! // We have a convenience function to only pick only a single suggestion, giving `Some` or `None`
+//! let single_suggestion = suggestions::provide_a_suggestion(input, &possible_vals);
+//! assert_eq!(single_suggestion.unwrap(), "test");
+//! ```
+//!
+//! ```
+//! let possible_vals = vec!["testing", "tempo"];
+//! let input = "teso"; // Sems ambiguous. Maybe multiple suggestions?
+//! let suggestions = suggestions::provide_suggestions(input, &possible_vals);
+//! // The implementation trys to order matches from "best" to "wort"
+//! assert_eq!(suggestions, vec!["testing", "tempo"]);
+//! // Asking for a single suggestion here would attempt to return the "best" one
+//! ```
+//!
+//! If nothing is reasonably similar, asking for suggestions
+//! will return `vec![]` or `None`.
+//!
+//! ```
+//! let possible_vals = vec!["testing", "things", "here"];
+//! let input = "--something-completely_different";
+//! assert_eq!(suggestions::provide_a_suggestion(&input, &possible_vals), None)
+//! ```
+#![deny(missing_docs)]
 use std::cmp::Ordering;
 use std::convert::AsRef;
 
 /// The confidence interval used to detect similarities.
 const CONFIDENCE_THRESHOLD: f64 = 0.8;
 
-/// Suggest a string that is similar to `target`,
+/// Suggest a string that most is similar to `target`,
 /// but is actually present in the `possible_values`.
 ///
-/// Returns `None` if there is nothing reasonably similar.
+/// Returns `None` if there is nothing in `possible_values` that is reasonably similar.
 ///
-/// For example, if the user types `--fod` but the possible values are `["foo", "bar"]`,
-/// then this function will suggest `Some("foo")`. If the user types 
-/// `--something-completely-different`, then this will return `None`.
+/// See also [provide_suggestions] to consider multiple possible suggestions (**recommended**).
 ///
-/// See also [provide_suggestions] to consider multiple possiblities.
-///
-/// If multiple possible values have the same level of (internal) similarity, this picks one
-/// arbitrarily (which is not the best UI).
-pub fn provide_suggestion<I, T>(target: &str, possible_values: I) -> Option<String>
+/// If there are multiple ,possible
+pub fn provide_a_suggestion<I, T>(target: &str, possible_values: I) -> Option<String>
     where T: AsRef<str>, I: IntoIterator<Item=T> {
     provide_suggestions(target, possible_values).pop()
 }
@@ -30,11 +55,10 @@ pub fn provide_suggestion<I, T>(target: &str, possible_values: I) -> Option<Stri
 ///
 /// Returns an empty vector `vec![]` if there is nothing reasonbly similar.
 ///
-/// For example, if the user types `--fod` but the possible values are `["foo", "bar"]`,
-/// then this function will suggest `["foo"]`. If the user types 
-/// `--something-completely-different`, then this will return `vec![]`.
+/// The implementation sorts suggestions based on its internal notion of similarity (from best ->
+/// worst).
 ///
-/// See also [provide_suggestions], which only picks a single suggesetion.
+/// See also [provide_suggestion], which only picks a single suggesetion.
 pub fn provide_suggestions<I, T>(target: &str, possible_values: I) -> Vec<String> 
     where T: AsRef<str>, I: IntoIterator<Item=T> {
     // Implementation copied directly from clap
@@ -47,28 +71,3 @@ pub fn provide_suggestions<I, T>(target: &str, possible_values: I) -> Vec<String
     candidates.into_iter().map(|(_, pv)| pv).collect()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    // Copied directly from Clap, with `did_you_mean` -> `provide_suggestions`
-    // See original: https://github.com/clap-rs/clap/blob/7b7c76e3d0279b474c774ea738aecb1d77251df8/src/parse/features/suggestions.rs#L78-L94
-
-    #[test]
-    fn possible_values_match() {
-        let p_vals = ["test", "possible", "values"];
-        assert_eq!(provide_suggestions("tst", p_vals.iter()), vec!["test"]);
-    }
-
-    #[test]
-    fn possible_values_match_multiple() {
-        let p_vals = ["test", "temp"];
-        assert_eq!(provide_suggestions("te", p_vals.iter()), vec!["test", "temp"]);
-    }
-
-    #[test]
-    fn possible_values_nomatch() {
-        let p_vals = ["test", "possible", "values"];
-        assert_eq!(provide_suggestions("hahaahahah", p_vals.iter()), Vec::<String>::new());
-    }
-
-}
